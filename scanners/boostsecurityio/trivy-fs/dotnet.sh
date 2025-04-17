5f45e8c749f03e1a3adc3e8d9847be15b1adecc3
François Lajeunesse-Robert <francois.lajeunesse-robert@boostsecurity.io>
2025-04-17 13:49:06 -0400

    Restore

--- scanners/boostsecurityio/trivy-fs/dotnet.sh
+++ scanners/boostsecurityio/trivy-fs/dotnet.sh
@@ -2,11 +2,6 @@ project_list=$(find . -name "*.csproj")
 
 nuget_cache=$(mktemp -d)
 
-log.info ()
-{ # $@=message
-  printf "$(date +'%H:%M:%S') [\033[34m%s\033[0m] %s\n" "INFO" "${*}";
-}
-
 for project_file in $project_list
 do
     project_location=$(dirname "$project_file")
@@ -15,7 +10,7 @@ do
     [ "$(find "$project_location" -name "*.deps.json" | wc -l)" != "0" ] && continue
     [ "$(find "$project_location" -name "*Packages.props" | wc -l)" != "0" ] && continue
 
-    log.info "Dotnet project without any lockfile. Restoring lockfile for $project_file"
+    # "Dotnet project without any lockfile. Restoring lockfile for $project_file"
 
     docker run --mount "type=bind,src=$project_location,dst=/tmp" \
                --mount "type=bind,src=$nuget_cache,dst=/root/.nuget" \
