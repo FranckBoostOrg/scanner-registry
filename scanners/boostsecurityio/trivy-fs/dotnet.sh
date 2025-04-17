fa8001c94b04f67a989b1f31b8aa7f16333c9ee4
François Lajeunesse-Robert <francois.lajeunesse-robert@boostsecurity.io>
2025-04-17 14:12:41 -0400

    Fixed permmissions

--- scanners/boostsecurityio/trivy-fs/dotnet.sh
+++ scanners/boostsecurityio/trivy-fs/dotnet.sh
@@ -15,8 +15,6 @@ do
     docker run --mount "type=bind,src=$project_location,dst=/tmp" \
                --mount "type=bind,src=$nuget_cache,dst=/root/.nuget" \
                --workdir /tmp \
-               --entrypoint dotnet \
-               mcr.microsoft.com/dotnet/sdk:9.0 restore -f --use-lock-file > /dev/null 2>&1
-
-    chmod o+rw $project_location/*
+               --entrypoint bash \
+               -c 'dotnet mcr.microsoft.com/dotnet/sdk:9.0 restore -f --use-lock-file && chmod o+rw *' > /dev/null 2>&1
 done
