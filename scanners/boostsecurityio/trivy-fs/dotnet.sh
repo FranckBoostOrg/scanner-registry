263a92033d55eb472e0ab5658f8fb82d5d053a75
François Lajeunesse-Robert <francois.lajeunesse-robert@boostsecurity.io>
2025-04-17 13:47:30 -0400

    Restore

--- scanners/boostsecurityio/trivy-fs/dotnet.sh
+++ scanners/boostsecurityio/trivy-fs/dotnet.sh
@@ -2,6 +2,11 @@ project_list=$(find . -name "*.csproj")
 
 nuget_cache=$(mktemp -d)
 
+log.info ()
+{ # $@=message
+  printf "$(date +'%H:%M:%S') [\033[34m%s\033[0m] %s\n" "INFO" "${*}";
+}
+
 for project_file in $project_list
 do
     project_location=$(dirname "$project_file")
@@ -10,11 +15,11 @@ do
     [ "$(find "$project_location" -name "*.deps.json" | wc -l)" != "0" ] && continue
     [ "$(find "$project_location" -name "*Packages.props" | wc -l)" != "0" ] && continue
 
-    echo "Dotnet project without any lockfile. Restoring lockfile"
+    log.info "Dotnet project without any lockfile. Restoring lockfile for $project_file"
 
     docker run --mount "type=bind,src=$project_location,dst=/tmp" \
                --mount "type=bind,src=$nuget_cache,dst=/root/.nuget" \
                --workdir /tmp \
                --entrypoint dotnet \
-               mcr.microsoft.com/dotnet/sdk:9.0 restore -f --use-lock-file
+               mcr.microsoft.com/dotnet/sdk:9.0 restore -f --use-lock-file > /dev/null 2>&1
 done
