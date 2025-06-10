01cf80cfe608d01591db0dbf3f477cde1890decf
Franck-Boost <francois.lajeunesse-robert@boostsecurity.io>
2025-06-10 16:35:58 -0400

    Update checks.sh
    Signed-off-by: Franck-Boost <francois.lajeunesse-robert@boostsecurity.io>

--- scanners/boostsecurityio/supply-chain-inventory/checks.sh
+++ scanners/boostsecurityio/supply-chain-inventory/checks.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "$(</dev/stdin)"
+STD_IN="$(</dev/stdin)"
+
+read -r -d '' VAR << EOM
+.runs[0].components += [
+        {
+          "purl": "pkg:generic/project1",
+          "name": "project1",
+          "namespace": "generic",
+          "provider": "csproj",
+          "type": "project_file",
+          "version": "128a63446a954579617e875aaab7d2978154e969",
+          "details": {
+            "type": "package",
+            "permissions": null,
+          "path": "project1.csproj"
+          }
+        }]
+EOM
+
+echo $STD_IN | jq --raw-output "$VAR"
