f93b7920dca3221014be98c0d77634aa4d2df7c0
François Lajeunesse-Robert <francois.lajeunesse-robert@boostsecurity.io>
2025-07-04 15:10:12 -0400

    Changed the semgrep custom rule validation logic

---
+++ scanners/boostsecurityio/semgrep/prescan_checks.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+local_rules_dst=$(mktemp -d)
+if [ -d ".semgrep" ]
+then
+    mv .semgrep/ "$local_rules_dst"
+fi
+mkdir -p .semgrep
+
+fetch_remote() {
+    dst_file=$(mktemp)
+    http_code=$(curl -s -L --fail -w '%{http_code}\n' -o $dst_file $1)
+    if [ "${http_code}" == "200" ]
+    then
+        cp $dst_file .semgrep/
+        rm -f $dst_file
+        return 0
+    fi
+
+    >&2 echo "Semgrep custom rules - Cannot fetch $1."
+    rm -f $dst_file
+    rm -rf $local_rules_dst
+    exit 1
+}
+
+SEMGREP_RULES=${SEMGREP_RULES:-boost/sast/rules/semgrep@stable}
+for rule in $SEMGREP_RULES; do
+    case "$rule" in
+      .semgrep/*)
+        # Local rules are allowed
+        cp -R $local_rules_dst/.semgrep/* .semgrep || true
+        ;;
+      http://*|https://*)
+        fetch_remote $rule
+        ;;
+      boost/sast/rules/semgrep@*)
+        # Boost
+        version=$(echo "$rule" | cut -d '@' -f 2)
+        fetch_remote "https://assets.build.boostsecurity.io/semgrep-rules/$version/all-sast-rules.yml"
+        ;;
+      *)
+        >&2 echo "Semgrep custom rules validation failed on $rule."
+        >&2 echo "  Community rules cannot be used."
+        >&2 echo "  Provide a URL or relative path to rules file or leave blank for Boost curated rules."
+        rm -rf $local_rules_dst
+        exit 1
+        ;;
+    esac
+done
+
+rm -rf $local_rules_dst
